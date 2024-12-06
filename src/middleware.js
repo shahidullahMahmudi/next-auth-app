50624dbe9ad051e7febb9f364633ea09d862d752
Mahmudi <shahidullah23122@gmail.com>
2024-12-06 11:59:09 +0600

    ffgg

--- src/middleware.js
+++ src/middleware.js
@@ -10,3 +10,17 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+
+// export default clerkMiddleware((auth, request) => {
+//   if(!isPublicRoute(request)) {
+//     auth().protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
